4e47cd1e4a883e215e5d32f07c7728afcb18e4c7
Madz96 <mad.subasinghe@gmail.com>
2021-05-18 14:19:56 +0530

    add return empty array on api error

--- client/src/components/ContactPage/api.js
+++ client/src/components/ContactPage/api.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export const fetchContactsAPI = async () => {
     const response = await axios.get('http://localhost:3100/contacts/getAllContacts').catch(error => {
         console.log("Error when fetching all contacts: ", error);
+        return { data: [] };
     });
 
     return response;
@@ -12,9 +13,10 @@ export const fetchContactsAPI = async () => {
 /* API call to retrieve contact by name */
 export const searchForContactAPI = async (query, type) => {
     const payload = { query: query, type: type };
-    
+
     const response = await axios.post('http://localhost:3100/contacts/searchByName', payload).catch(error => {
         console.log("Error when fetching searched contacts: ", error);
+        return { data: [] };
     });
 
     return response;
